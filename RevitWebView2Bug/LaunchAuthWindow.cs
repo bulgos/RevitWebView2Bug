f70fae2747e153c23a3a8be5e9bcfbe509e8e2cd
Daniel Christev <christev@designtoproduction.com>
2025-01-30 20:47:16 +0100

    Add project files.

---
+++ RevitWebView2Bug/LaunchAuthWindow.cs
@@ -0,0 +1,20 @@
+﻿using Auth0WPF.Core;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitWebView2Bug
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class LaunchAuthWindowCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
